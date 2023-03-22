6b2f908604b0a6c89630c40968cfa033b3236835
c100bit <c100bit@gmail.com>
2023-03-22 18:42:55 +0300

    beans xml

---
+++ src/main/java/org/example/ClassicalMusic.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    @Override
+    public String getSong() {
+        return "Rhapsody";
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing my init");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destroy");
+    }
+
+}
---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+    private Music music;
+    private String name;
+    private int volume;
+
+    private List<Music> list = new ArrayList<>();
+
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {
+    }
+
+    public void setList(List<Music> list) {
+        this.list = list;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing" + music.getSong());
+    }
+
+    public void playMusicList() {
+        for (Music song : list) {
+            System.out.println("Playing" + song.getSong());
+        }
+    }
+}
